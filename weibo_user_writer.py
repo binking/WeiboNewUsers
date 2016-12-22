556140d046548b1252cb01f7a93ee655cf92ade0
binking <jiangzhibin2014.xujie@gmail.com>
2016-12-22 14:49:10 +0800

    debug

--- weibo_user_writer.py
+++ weibo_user_writer.py
@@ -75,13 +75,56 @@ class WeiboUserWriter(DBAccesor):
             AND t.topic_url = twr.topic_url
             AND twr.weibo_url = wc.weibo_url
             ) AS CommentAuthor LEFT JOIN WeiboUser wu ON CommentAuthor.weibocomment_author_url = wu.weibo_user_url 
-            WHERE wu.weibo_user_url IS NULL
+            WHERE wu.weibo_user_url IS NULL LIMIT 10
         """
         conn = self.connect_database()
         cursor = conn.cursor()
         cursor.execute(select_new_user_sql)
         for res in cursor.fetchall():
             yield res[0]
+
+"""
+What is cursor ???
+_result None
+description None
+rownumber 0
+messages []
+_executed 
+            INSERT INTO WeiboUser (clueid, fullpath, realpath, 
+            theme,  middle, createdate, bucketName,
+            uri, weibo_user_url, nickname, gender, introduction, realname, location, 
+            registration_date, label, date_of_birth, company,preliminary_school,
+            middle_school, high_school, tech_school, university, blog_url, domain, 
+            msn, QQ, email, sex_tendancy, emotion, blood_type, focus_num, fans_num, 
+            weibo_num , KOL)
+            SELECT '','http://weibo.com/1681897083/info','http://weibo.com/1681897083/info','新浪微博_博主详细信息48992_daily','default','2016-12-22 14:00:57','follower','http://weibo.com/1681897083/info','http://weibo.com/1681897083','连慧勇N',
+            '男','海纳百川','','其他','2011-11-11','','','','','','','',
+            '河南工程学院','','','','','','','','',343,187,211,''
+            FROM DUAL WHERE NOT EXISTS(SELECT * FROM WeiboUser WHERE weibo_user_url = 'http://weibo.com/1681897083/info')
+        
+errorhandler <bound method Connection.defaulterrorhandler of <_mysql.connection open to '10.66.110.147' at 1358eb0>>
+rowcount 1
+connection <_mysql.connection open to '10.66.110.147' at 1358eb0>
+description_flags None
+arraysize 1
+_info Records: 1  Duplicates: 0  Warnings: 0
+lastrowid 4604478
+_last_executed 
+            INSERT INTO WeiboUser (clueid, fullpath, realpath, 
+            theme,  middle, createdate, bucketName,
+            uri, weibo_user_url, nickname, gender, introduction, realname, location, 
+            registration_date, label, date_of_birth, company,preliminary_school,
+            middle_school, high_school, tech_school, university, blog_url, domain, 
+            msn, QQ, email, sex_tendancy, emotion, blood_type, focus_num, fans_num, 
+            weibo_num , KOL)
+            SELECT '','http://weibo.com/1681897083/info','http://weibo.com/1681897083/info','新浪微博_博主详细信息48992_daily','default','2016-12-22 14:00:57','follower','http://weibo.com/1681897083/info','http://weibo.com/1681897083','连慧勇N',
+            '男','海纳百川','','其他','2011-11-11','','','','','','','',
+            '河南工程学院','','','','','','','','',343,187,211,''
+            FROM DUAL WHERE NOT EXISTS(SELECT * FROM WeiboUser WHERE weibo_user_url = 'http://weibo.com/1681897083/info')
+        
+_warnings 0
+_rows ()
+"""
 
 # insert_new_user_sql = """
 #     INSERT INTO weibouser
